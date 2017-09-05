7438986cbfcf9a94b7d2c500ea459a197b63eefd
PatricioRamirezV <patric.ramirezv@gmail.com>
2017-09-05 14:58:52 -0300

    Defectos solucionados if, eval,..

--- calculador.js
+++ calculador.js
@@ -1,54 +1,61 @@
 function calcular(figura){
-    if (figura=='circulo'){
-        var radio = document.calculadora.radio.value
-        var result = eval(radio*radio*3.1416)
-        document.calculadora.resultado.value = result
+    switch (figura){
+        case 'circulo':
+            var radio = document.calculadora.radio.value
+            var result = radio*radio*3.1416
+            document.calculadora.resultado.value = result
+        break;
+        case 'cuadrilatero':
+            var largo = document.calculadora.largo.value
+            var ancho = document.calculadora.ancho.value
+            var result = largo*ancho
+            document.calculadora.resultado.value = result
+        break;
+        case 'triangulo':
+            var base = document.calculadora.base.value
+            var altura = document.calculadora.altura.value
+            var result = (base*altura)/2
+            document.calculadora.resultado.value = result
+        break;
+
     }
-    if (figura=='cuadrilatero'){
-        var largo = document.calculadora.largo.value
-        var ancho = document.calculadora.ancho.value
-        var result = eval(largo*ancho)
-        document.calculadora.resultado.value = result
-    }
-     if (figura=='triangulo'){
-         var base = document.calculadora.base.value
-    var altura = document.calculadora.altura.value
-    var result = eval((base*altura)/2)
-    document.calculadora.resultado.value = result
-    }
+   
 
 }
 function clean(value) {
-    if(value=='radio'){
-        document.calculadora.resultado.value='';
-        document.calculadora.largo.value='';
-        document.calculadora.ancho.value='';
-        document.calculadora.base.value='';
-        document.calculadora.altura.value='';
-    }
-    if(value=='largo'){
-        document.calculadora.resultado.value='';
-        document.calculadora.radio.value='';
-        document.calculadora.base.value='';
-        document.calculadora.altura.value='';
-    }
-    if(value=='ancho'){
-        document.calculadora.resultado.value='';
-        document.calculadora.radio.value='';
-        document.calculadora.base.value='';
-        document.calculadora.altura.value='';
-    }
-    if(value=='altura'){
-        document.calculadora.resultado.value='';
-        document.calculadora.radio.value='';
-        document.calculadora.largo.value='';
-        document.calculadora.ancho.value='';
-    }
-    if(value=='base'){
-        document.calculadora.resultado.value='';
-        document.calculadora.radio.value='';
-        document.calculadora.largo.value='';
-        document.calculadora.ancho.value='';
+    switch (value){
+        case'radio':
+            document.calculadora.resultado.value='';
+            document.calculadora.largo.value='';
+            document.calculadora.ancho.value='';
+            document.calculadora.base.value='';
+            document.calculadora.altura.value='';
+        break;
+        case'largo':
+            document.calculadora.resultado.value='';
+            document.calculadora.radio.value='';
+            document.calculadora.base.value='';
+            document.calculadora.altura.value='';
+        break;
+        case'ancho':
+            document.calculadora.resultado.value='';
+            document.calculadora.radio.value='';
+            document.calculadora.base.value='';
+            document.calculadora.altura.value='';
+        break;
+        case'altura':
+            document.calculadora.resultado.value='';
+            document.calculadora.radio.value='';
+            document.calculadora.largo.value='';
+            document.calculadora.ancho.value='';
+        break;
+        case'base':
+            document.calculadora.resultado.value='';
+            document.calculadora.radio.value='';
+            document.calculadora.largo.value='';
+            document.calculadora.ancho.value='';
+        break;
+
     }
     
     
