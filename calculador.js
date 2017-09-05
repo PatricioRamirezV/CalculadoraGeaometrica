60654384917fc00b31c7656cfc5da3c7d845a1a5
PatricioRamirezV <patric.ramirezv@gmail.com>
2017-09-05 15:16:24 -0300

    returns y semiclon solucionados

--- calculador.js
+++ calculador.js
@@ -8,55 +8,54 @@ function calcular(figura){
         case 'cuadrilatero':
             var largo = document.calculadora.largo.value
             var ancho = document.calculadora.ancho.value
-            var result = largo*ancho
+            result = largo*ancho
             document.calculadora.resultado.value = result
         break;
         case 'triangulo':
             var base = document.calculadora.base.value
             var altura = document.calculadora.altura.value
-            var result = (base*altura)/2
+            result = (base*altura)/2
             document.calculadora.resultado.value = result
         break;
 
     }
+    return;
    
 
 }
 function clean(value) {
+
+    document.calculadora.resultado.value='';
     switch (value){
         case'radio':
-            document.calculadora.resultado.value='';
             document.calculadora.largo.value='';
             document.calculadora.ancho.value='';
             document.calculadora.base.value='';
             document.calculadora.altura.value='';
         break;
         case'largo':
-            document.calculadora.resultado.value='';
             document.calculadora.radio.value='';
             document.calculadora.base.value='';
             document.calculadora.altura.value='';
         break;
         case'ancho':
-            document.calculadora.resultado.value='';
             document.calculadora.radio.value='';
             document.calculadora.base.value='';
             document.calculadora.altura.value='';
         break;
         case'altura':
-            document.calculadora.resultado.value='';
             document.calculadora.radio.value='';
             document.calculadora.largo.value='';
             document.calculadora.ancho.value='';
         break;
         case'base':
-            document.calculadora.resultado.value='';
             document.calculadora.radio.value='';
             document.calculadora.largo.value='';
             document.calculadora.ancho.value='';
         break;
 
     }
+    return ;
     
     
 }
